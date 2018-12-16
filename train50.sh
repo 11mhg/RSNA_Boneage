5924c60fef1c253b6d75221d1f783648b5d8abb2
Mohammed Gasmallah <mhg1@gra-login3.graham.sharcnet>
2018-12-16 10:24:55 -0500

    Final final commit

--- train50.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --job-name=train-50
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=30GB
-#SBATCH --gres=gpu:1
-#SBATCH --time=1-0
-
-srun --gres=gpu:2 python3 ./main.py -td ./ -e 60
-
